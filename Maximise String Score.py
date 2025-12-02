26d0ebbbbee4f9874e77630e60f85464d35fa687
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2025-12-02 01:52:29 -0500

    Update Maximise String Score.py

--- Maximise String Score.py
+++ Maximise String Score.py
@@ -1,16 +1,15 @@
+import bisect
 class Solution:
     def maxScore(self, s, jumps):
         # code here
         from collections import defaultdict
         n=len(s)
-        
         can=defaultdict(set)
         for a,b in jumps:
             can[a].add(b)
         pos=defaultdict(list)
         for i,c in enumerate(s):
             pos[c].append(i)
-        
         pref=[0] * (n+1)
         for i in range(n):
             pref[i+1] = pref[i] + ord(s[i])
@@ -22,27 +21,29 @@ class Solution:
             prefChar[s[i]][i+1] += 1
         dp=[-1] * n
         dp[0]=0
-        
         for i in range(n):
             if dp[i]<0:
                 continue
             c1=s[i]
-            
-            for j in pos[c1]:
-                 if j > i:
-                     t = s[j]
-                     total = pref[j] - pref[i]
-                     remove=(prefChar[t][j] - prefChar[t][i]) * ord(t)
-                     score = total - remove
-                     dp[j] = max(dp[j],dp[i]+score)
+            lst = pos[c1]
+            idx = bisect.bisect_right(lst,i)
+            if idx < len(lst):
+                j=lst[idx]
+                t=s[j]
+                total = pref[j] - pref[i]
+                remove=(prefChar[t][j] - prefChar[t][i]) * ord(t)
+                score = total - remove
+                dp[j] = max(dp[j],dp[i]+score)
             for ch in can[c1]:
-                for j in pos[ch]:
-                    if j>i:
-                        t=s[j]
-                        total=pref[j] - pref[i]
-                        remove = (prefChar[t][j] - prefChar[t][i]) * ord(t)
-                        score = total - remove
-                        dp[j] = max(dp[j],dp[i]+score)
+                lst = pos[ch]
+                idx = bisect.bisect_right(lst,i)
+                if idx < len(lst):
+                    j=lst[idx]
+                    t=s[j]
+                    total=pref[j] - pref[i]
+                    remove = (prefChar[t][j] - prefChar[t][i]) * ord(t)
+                    score = total - remove
+                    dp[j] = max(dp[j],dp[i]+score)
         return max(dp)
                      
             
